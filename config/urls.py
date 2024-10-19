6cbeea91ba6e9f428ab8b5c2a7fa1c67180bb1fb
Dmitriy Sulzhits <dsulzhits@gmail.com>
2024-10-19 12:34:06 +0300

    Добавлены users.urls

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('users/', include('users.urls', namespace='users')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls', namespace='users')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
