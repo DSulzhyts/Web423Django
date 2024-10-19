889dc6fe32d8aa7e400c5917f6f155d627bf051b
Dmitriy Sulzhits <dsulzhits@gmail.com>
2024-10-19 12:54:27 +0300

    Добавлена команда для создания суперпользователя.

---
+++ users/management/commands/ccsu.py
@@ -0,0 +1,23 @@
+import os
+
+from django.core.management import BaseCommand
+from dotenv import load_dotenv
+
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email='admin@web.top',
+            first_name='Admin',
+            last_name='Adminov',
+            is_staff=True,
+            is_superuser=True,
+            is_active=True
+        )
+        load_dotenv()
+        user.set_password(os.getenv('SU_PASSWORD'))
+        user.save()
+        return 'Admin created'
