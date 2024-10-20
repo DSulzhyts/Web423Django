c15d90c200231d215a77526064385f7daa22b06c
Dmitriy Sulzhits <dsulzhits@gmail.com>
2024-10-20 12:16:26 +0300

    Добавлен класс-миксин для работы с формами.

--- users/forms.py
+++ users/forms.py
@@ -5,7 +5,14 @@ from users.models import User
 from users.validators import validate_password
 
 
-class UserRegisterForm(forms.ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class UserRegisterForm(StyleFormMixin, forms.ModelForm):
     password = forms.CharField(label='Password', widget=forms.PasswordInput, min_length=6, max_length=12,
                                help_text='От 6 до 12 букв латинского алфавита и/или цифр')
     password2 = forms.CharField(label='Repeat password', widget=forms.PasswordInput)
@@ -23,12 +30,12 @@ class UserRegisterForm(forms.ModelForm):
         return temp_data['password2']
 
 
-class UserLoginForm(forms.Form):
+class UserLoginForm(StyleFormMixin, forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
 
 
-class UserUpdateForm(forms.ModelForm):
+class UserUpdateForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name', 'phone', 'avatar',)
