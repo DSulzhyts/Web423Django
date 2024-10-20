22736fa15df5ede1b7fcb5a834ef3f37c1da3478
Dmitriy Sulzhits <dsulzhits@gmail.com>
2024-10-20 11:21:24 +0300

    Добавлен view для регистрации пользователя, прописан url, добавлен шаблон.

--- users/views.py
+++ users/views.py
@@ -6,4 +6,15 @@ from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 
+from users.forms import UserRegisterForm
 
+
+def user_register_view(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            return HttpResponseRedirect('users:login')
+    return render(request, 'users/register.html')
